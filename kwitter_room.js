64f7c9dc3befaa8012f354ef8accbf94e1d6534a
Rithwik13 <101996088+Rithwik13@users.noreply.github.com>
2022-07-13 13:56:20 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -4,7 +4,25 @@
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      firebase.database().ref(Room_name).push({
+        name:user_name,
+        message:msg,
+        like:0
+      });
       //End code
       });});}
 getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+        window.location = "kwitter_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "kwitter.html";      
+}
+
